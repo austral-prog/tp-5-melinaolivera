1724d2b15cc7b47e1f79a890b6537ba40f3e3a48
melinaolivera <162723084+melinaolivera@users.noreply.github.com>
2024-04-25 22:52:23 -0300

    Update number_to_month.py

--- number_to_month.py
+++ number_to_month.py
@@ -1,4 +1,43 @@
 # Replace the "ANSWER HERE" for your answer
-
 def number_to_month(month):
-    return "ANSWER HERE" # Remove this line and implement
+    if month  == 1:
+        return "enero"
+    elif month == 2:
+        return "febrero"
+    elif month == 3:
+        return "marzo"
+    elif month == 4:
+        return "abril"
+    elif month == 5:
+        return "mayo"
+    elif month == 6:
+        return "junio"
+    elif month == 7:
+        return "julio"
+    elif month == 8:
+        return "agosto"
+    elif month == 9:
+        return "septiembre"
+    elif month == 10:
+        return "octubre"
+    elif month == 11:
+        return "noviembre"
+    elif month == 12:
+        return "diciembre"
+    else:
+        return 'error'
+    
+    
+    number_to_month(1)
+    number_to_month(2)
+    number_to_month(3)
+    number_to_month(4)
+    number_to_month(5)
+    number_to_month(6)
+    number_to_month(7)
+    number_to_month(8)
+    number_to_month(9)
+    number_to_month(10)
+    number_to_month(11)
+    number_to_month(12)
+    
